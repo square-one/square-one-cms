7530a2489d739be8f7d15316cc8fef0b75c7409a
Jeremy Wilken <gnomation@gnomeontherun.com>
2011-12-16 22:53:11 -0600

    Build now should generate a changelog through Jenkins and Phing

---
+++ build/phing/tasks/squareone/ChangelogTask.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @author      Jeremy Wilken - Gnome on the run
+ * @link        www.gnomeontherun.com
+ * @copyright   Copyright 2011 Gnome on the run. All Rights Reserved.
+ * @category    
+ * @package     
+ */
+
+require_once "/usr/share/php/phing/Task.php";
+
+class ChangelogTask extends Task {
+    
+    /**
+     * Changelog file
+     */
+    private $tofile = 'installation/CHANGELOG';
+    
+    /**
+     * Changelog template
+     */
+    private $template = 'squareone.txt';
+    
+    /**
+     * Setter for $filename
+     */
+    public function setTofile($str)
+    {
+        $this->tofile = $str;
+    }
+    
+    /**
+     * Setter for $filename
+     */
+    public function setTemplate($str)
+    {
+        $this->template = $str;
+    }
+    
+    /**
+     * init step
+     */
+    public function init()
+    {
+        
+    }
+    
+    /**
+     * Main function
+     */
+    public function main()
+    {
+        $this->log('Starting changelog generation, please wait...');
+        
+        $command = 'git log --no-merges --format="%cd" --date=short | sort -u -r | while read DATE ; do
+    echo
+    echo [$DATE]
+    GIT_PAGER=cat git log --no-merges --format=" * %s" --since=$DATE --until=$NEXT
+    NEXT=$DATE
+    done;';
+        
+        exec($command, $response);
+        
+        $template = file_get_contents(dirname(__FILE__).'/changelog/'.$this->template);
+        
+        $data = $template.implode(PHP_EOL, $response);
+        
+        file_put_contents($this->tofile, $data);
+        
+        $this->log('Generated changelog at ' . $this->tofile);
+    }
+}
