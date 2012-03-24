0d687d1291df4b685929f24c02788ada8ca1ff77
Joe LeBlanc <contact@jlleblanc.com>
2012-03-23 23:20:55 -0500

    Added support for CDNs.

--- plugins/system/jslibs/jslibs.php
+++ plugins/system/jslibs/jslibs.php
@@ -21,7 +21,7 @@ class plgSystemJslibs extends JPlugin
 	 */
 	public function onBeforeCompileHead()
 	{
-		JavascriptLibraries::seed_document_head();
+		JavascriptLibraries::seed_document_head($this->params);
 
 		return true;
 	}
@@ -151,11 +151,14 @@ class JavascriptLibraries
 	 * head section of the document. The minimum acceptable version of the
 	 * JavaScript library is used.
 	 *
+	 * @param JRegistry $params - Object with the plugin's configuration info
 	 * @return void
 	 * @throws JavascriptException
 	 */
-	static public function seed_document_head()
+	static public function seed_document_head($params)
 	{
+		$cdn = new JavascriptLibrariesCDN($params);
+
 		foreach (self::$libraries as $lib_name => $records) {
 
 			$min_version = '0.0.0';
@@ -177,14 +180,25 @@ class JavascriptLibraries
 			}
 
 			/**
+			 * Get the CDN URL for the library. If none is available, fallback.
+			 * to the local copy
+			 *
+			 */
+			$lib_path = $cdn->get_cdn_url($lib_name, $min_version);
+
+			if (!$lib_path) {
+				$lib_path = $records[$min_version];
+			}
+
+			/**
 			 * TODO: handle CDNs. Adding the script to the document object may
 			 * ultimately work best as a separate function.
 			 *
 			 */
 			if ($lib_name == 'jquery') {
-				self::add_jquery($records[$min_version]);
+				self::add_jquery($lib_path);
 			} else {
-				self::add_javascript_path($records[$min_version]);
+				self::add_javascript_path($lib_path);
 			}
 
 			/**
@@ -238,5 +252,115 @@ class JavascriptLibraries
 		} else {
 			$doc->addScript(JURI::base() . $path);
 		}
+	}
+}
+
+class JavascriptLibrariesCDN
+{
+	public $bases = array(
+		'google' => 'ajax.googleapis.com/ajax/libs/',
+		'microsoft' => 'ajax.aspnetcdn.com/ajax/'
+	);
+
+	public $min_versions = array(
+		'google' => array(
+			'chrome-frame' => '1.0.0',
+			'dojo' => '1.1.1',
+			'ext-core' => '3.0.0',
+			'jquery' => '1.2.3',
+			'jqueryui' => '1.5.2',
+			'mootools' => '1.1.1',
+			'prototype' => '1.6.0.2',
+			'scriptaculous' => '1.8.1',
+			'swfobject' => '2.1',
+			'webfont' => '1.0.0'
+		),
+		'microsoft' => array( // uses dots instead of dashes, and jqueryui is jquery.ui
+			'jquery' => '1.3.2',
+			'jqueryui' => '1.8.5'
+		)
+	);
+
+	public $cdn_formats = array(
+		'google' => array(
+			'chrome-frame' => 'chrome-frame/%s/CFInstall.min.js',
+			'dojo' => 'dojo/%s/dojo/dojo.js',
+			'ext-core' => 'ext-core/%s/ext-core.js',
+			'jquery' => 'jquery/%s/jquery.min.js',
+			'jqueryui' => 'jqueryui/%s/jquery-ui.min.js',
+			'mootools' => 'mootools/%s/mootools-yui-compressed.js',
+			'prototype' => 'prototype/%s/prototype.js',
+			'scriptaculous' => 'scriptaculous/%s/scriptaculous.js',
+			'swfobject' => 'swfobject/%s/swfobject.js',
+			'webfont' => 'webfont/%s/webfont.js'
+		),
+		'microsoft' => array(
+			'jquery' => 'jQuery/jquery-%s.min.js',
+			'jqueryui' => 'jquery.ui/%s/jquery-ui.min.js'
+		)
+	);
+
+	public $params; // Plugin preferences
+	public $overrides;
+
+	public function __construct($params)
+	{
+		$this->params = $params;
+	}
+
+	public function get_cdn_url($library, $version)
+	{
+		// check for override, if it exists for the library, use regardless of version
+		$override = $this->get_cdn_override($library);
+
+		if ($override) {
+			return $override;
+		}
+
+		// check for the selected CDN. If 'none' is selected, skip the rest
+		$selected_cdn = $this->params->get('cdn');
+
+		if ($selected_cdn == '') {
+			return false;
+		}
+
+		return $this->make_cdn_url($selected_cdn, $library, $version);
+	}
+
+	public function make_cdn_url($cdn, $library, $version)
+	{
+		if (!isset($this->min_versions[$cdn][$library])) {
+			return false;
+		}
+
+		if (version_compare($version, $this->min_versions[$cdn][$library], '<')) {
+			return false;
+		}
+
+		$uri = JURI::getInstance();
+
+		return $uri->getScheme() . '://' . $this->bases[$cdn] . sprintf($this->cdn_formats[$cdn][$library], $version);
+	}
+
+	public function get_cdn_override($library)
+	{
+		if (!isset($this->overrides)) {
+			$this->overrides = array();
+
+			$lines = explode("\n", $this->params->get('overrides'));
+
+			foreach ($lines as $line) {
+				if (strlen($line)) {
+					list($lib, $url) = explode(',', $line);
+					$this->overrides[$lib] = $url;
+				}
+			}
+		}
+
+		if (isset($this->overrides[$library])) {
+			return $this->overrides[$library];
+		}
+
+		return false;
 	}
 }
